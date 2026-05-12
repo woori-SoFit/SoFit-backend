946bea3a0b50d7bc4f05c4bd0a31caf71dbe7af9
noiskk <zionzion00@naver.com>
2026-05-12 17:07:29 +0900

    init: Spring Boot 프로젝트 초기 설정

---
+++ sofit-admin/src/main/java/com/sofit/admin/SofitAdminApplication.java
@@ -0,0 +1,11 @@
+package com.sofit.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SofitAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SofitAdminApplication.class, args);
+    }
+}
---
+++ sofit-user/src/main/java/com/sofit/user/SofitUserApplication.java
@@ -0,0 +1,11 @@
+package com.sofit.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SofitUserApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SofitUserApplication.class, args);
+    }
+}
