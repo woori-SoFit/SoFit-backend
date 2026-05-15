27882caa77101e8a4418697844ff669becb96482
noiskk <zionzion00@naver.com>
2026-05-15 09:56:19 +0900

    [SOFIT-32] Feat: 사용자 본인 심사 중인 대출 목록/상세 조회 API 구현

--- sofit-user/src/main/java/com/sofit/user/SofitUserApplication.java
+++ sofit-user/src/main/java/com/sofit/user/SofitUserApplication.java
@@ -2,8 +2,15 @@ package com.sofit.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.sofit")
+@EntityScan("com.sofit.common.entity")
+@EnableJpaRepositories(basePackages = {
+        "com.sofit.common.repository",
+        "com.sofit.user"
+})
 public class SofitUserApplication {
     public static void main(String[] args) {
         SpringApplication.run(SofitUserApplication.class, args);
