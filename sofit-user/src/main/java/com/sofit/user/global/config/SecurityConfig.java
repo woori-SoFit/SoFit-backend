ee31d0df0f770ce870b2fe0b0a6a8a6ac545ec7a
HeeYeon-Ko <rhgmldus6@gmail.com>
2026-05-15 16:28:37 +0900

    Chore: WebConfig의 CORS 설정

--- sofit-user/src/main/java/com/sofit/user/global/config/SecurityConfig.java
+++ sofit-user/src/main/java/com/sofit/user/global/config/SecurityConfig.java
@@ -14,6 +14,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(cors -> {})
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().permitAll()
