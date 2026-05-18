2c2347487ba7d38b5644b2fbbc2843ee7af59ef3
light11014 <dtq1014@naver.com>
2026-05-18 22:25:20 +0900

    [SOFIT-20] Feat: AuthService 로그인 로직 구현

--- sofit-user/src/main/java/com/sofit/user/domain/auth/service/AuthService.java
+++ sofit-user/src/main/java/com/sofit/user/domain/auth/service/AuthService.java
@@ -2,12 +2,17 @@ package com.sofit.user.domain.auth.service;
 
 import com.sofit.user.domain.auth.dto.request.BusinessVerificationRequest;
 import com.sofit.user.domain.auth.dto.request.FinancialCertVerifyRequest;
+import com.sofit.user.domain.auth.dto.request.LoginRequest;
 import com.sofit.user.domain.auth.dto.response.BusinessVerificationResponse;
 import com.sofit.user.domain.auth.dto.response.FinancialCertVerifyResponse;
+import com.sofit.user.domain.auth.dto.response.LoginResponse;
+import jakarta.servlet.http.HttpSession;
 
 public interface AuthService {
 
     BusinessVerificationResponse verifyBusiness(BusinessVerificationRequest request);
 
     FinancialCertVerifyResponse verifyFinancialCertificate(FinancialCertVerifyRequest request);
+
+    LoginResponse login(LoginRequest request, HttpSession session);
 }
