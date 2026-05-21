0d0f9342bf524d814ec9554b5b66b312846bf600
HeeYeon-Ko <rhgmldus6@gmail.com>
2026-05-21 16:03:02 +0900

    [SOFIT-31] LoanApplicationSubmitRequest 순서 변경

--- sofit-user/src/main/java/com/sofit/user/domain/loan/dto/request/LoanApplicationSubmitRequest.java
+++ sofit-user/src/main/java/com/sofit/user/domain/loan/dto/request/LoanApplicationSubmitRequest.java
@@ -11,17 +11,18 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class LoanApplicationSubmitRequest {
 
-    @NotNull(message = "희망 대출 금액은 필수입니다.")
-    @Positive(message = "희망 대출 금액은 0보다 커야 합니다.")
-    private Long requestedAmount;
+    @NotNull(message = "대출 용도는 필수입니다.")
+    private LoanPurpose purpose;
+
+    @NotNull(message = "상환 방식은 필수입니다.")
+    private RepaymentMethod repaymentMethod;
 
     @NotNull(message = "희망 대출 기간은 필수입니다.")
     @Positive(message = "희망 대출 기간은 0보다 커야 합니다.")
     private Integer requestedTerm;
 
-    @NotNull(message = "상환 방식은 필수입니다.")
-    private RepaymentMethod repaymentMethod;
-
-    @NotNull(message = "대출 용도는 필수입니다.")
-    private LoanPurpose purpose;
+    @NotNull(message = "희망 대출 금액은 필수입니다.")
+    @Positive(message = "희망 대출 금액은 0보다 커야 합니다.")
+    private Long requestedAmount;
+   
 }
