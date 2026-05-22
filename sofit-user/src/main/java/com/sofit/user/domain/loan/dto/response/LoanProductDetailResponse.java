e406f97b3da5a2bc6e61d59879eca4d18a242898
HeeYeon-Ko <rhgmldus6@gmail.com>
2026-05-22 14:42:12 +0900

    [SOFIT-28] 응답 데이터 추가

--- sofit-user/src/main/java/com/sofit/user/domain/loan/dto/response/LoanProductDetailResponse.java
+++ sofit-user/src/main/java/com/sofit/user/domain/loan/dto/response/LoanProductDetailResponse.java
@@ -1,5 +1,6 @@
 package com.sofit.user.domain.loan.dto.response;
 
+import com.sofit.common.entity.loan.enums.IncomeType;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -17,7 +18,17 @@ public class LoanProductDetailResponse {
     private Long maxLimit;
     private Integer maxTerm;
     private String targetDescription;
+    private FilterConditions filterConditions;
     private InterestRate interestRate;
+
+    @Getter
+    @Builder
+    public static class FilterConditions {
+        private BigDecimal annualIncomeLimit;
+        private Short creditScoreLimit;
+        private IncomeType incomeTypeCodeLimit;
+        private BigDecimal existingLoanAmtLimit;
+    }
 
     @Getter
     @Builder
