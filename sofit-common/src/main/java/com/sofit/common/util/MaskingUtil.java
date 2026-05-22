989bdf5488b49e5134b05cece2bf044a6318049f
minyeong <skaalsdud7@gmail.com>
2026-05-22 14:29:02 +0900

    [SOFIT-116] Refactor: MaskingUtil, FormatUtil을 sofit-common으로 이동 및 분리

--- sofit-common/src/main/java/com/sofit/common/util/MaskingUtil.java
+++ sofit-common/src/main/java/com/sofit/common/util/MaskingUtil.java
@@ -1,7 +1,7 @@
-package com.sofit.user.global.util;
+package com.sofit.common.util;
 
 /**
- * 개인정보 마스킹 및 포맷팅 유틸리티
+ * 개인정보 마스킹 유틸리티
  */
 public class MaskingUtil {
 
@@ -23,23 +23,5 @@ public class MaskingUtil {
         }
 
         return residentNumber.substring(0, 6) + "-" + residentNumber.charAt(6) + "******";
-    }
-
-    /**
-     * 전화번호 포맷팅 (하이픈 삽입)
-     * - null/빈 문자열 → 빈 문자열 반환
-     * - 11자리 → NNN-NNNN-NNNN 형식
-     * - 그 외 → 원본 그대로 반환
-     */
-    public static String formatPhoneNumber(String phoneNumber) {
-        if (phoneNumber == null || phoneNumber.isEmpty()) {
-            return "";
-        }
-
-        if (phoneNumber.length() == 11) {
-            return phoneNumber.substring(0, 3) + "-" + phoneNumber.substring(3, 7) + "-" + phoneNumber.substring(7);
-        }
-
-        return phoneNumber;
     }
 }
