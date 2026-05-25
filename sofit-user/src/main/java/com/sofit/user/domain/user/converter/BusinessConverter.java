859c1993b6c997c45fa3de627c0f4a7a02f57642
hyeyoon23 <109073947+hyeyoon23@users.noreply.github.com>
2026-05-25 14:47:00 +0900

    [SOFIT-25] Feat: 사업자 정보 조회 API 구현

---
+++ sofit-user/src/main/java/com/sofit/user/domain/user/converter/BusinessConverter.java
@@ -0,0 +1,29 @@
+package com.sofit.user.domain.user.converter;
+
+import com.sofit.common.entity.auth.BusinessProfile;
+import com.sofit.common.entity.user.User;
+import com.sofit.common.util.FormatUtil;
+import com.sofit.common.util.MaskingUtil;
+import com.sofit.user.domain.user.dto.response.BusinessProfileResponse;
+
+public class BusinessConverter {
+
+    private BusinessConverter() {}
+
+    /**
+     * BusinessProfile 엔티티 → BusinessProfileResponse 변환
+     */
+    public static BusinessProfileResponse toBusinessProfileResponse(BusinessProfile profile) {
+        User user = profile.getUser();
+        return new BusinessProfileResponse(
+                FormatUtil.formatBusinessNumber(profile.getBusinessNumber()),
+                profile.getBusinessName(),
+                profile.getRepresentativeName(),
+                MaskingUtil.maskResidentNumber(user.getResidentNumber()),
+                profile.getOpenDate(),
+                profile.getBusinessCategory(),
+                profile.getBusinessType(),
+                profile.getBusinessAddress()
+        );
+    }
+}
---
+++ sofit-user/src/main/java/com/sofit/user/domain/user/exception/BusinessSuccessCode.java
@@ -0,0 +1,17 @@
+package com.sofit.user.domain.user.exception;
+
+import com.sofit.common.apiPayload.code.BaseSuccessCode;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum BusinessSuccessCode implements BaseSuccessCode {
+
+    BUSINESS_PROFILE_OK(HttpStatus.OK, "BUSINESS2001", "사업자 정보 조회에 성공했습니다.");
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+}
