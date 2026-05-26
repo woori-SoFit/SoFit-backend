d7c1d0fbafa7abaa70b8aa2a130ed7ba44521ee3
minyeong <skaalsdud7@gmail.com>
2026-05-26 19:36:24 +0900

    [SOFIT-133] Feat: 대출 신청 상세 조회 Response DTO 및 에러/성공 코드 정의

--- sofit-admin/src/main/java/com/sofit/admin/domain/loan/exception/LoanDashboardErrorCode.java
+++ sofit-admin/src/main/java/com/sofit/admin/domain/loan/exception/LoanDashboardErrorCode.java
@@ -9,7 +9,8 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum LoanDashboardErrorCode implements BaseErrorCode {
 
-    INVALID_STATUS_FILTER(HttpStatus.BAD_REQUEST, "LOAN4001", "유효하지 않은 심사 상태입니다.");
+    INVALID_STATUS_FILTER(HttpStatus.BAD_REQUEST, "LOAN4001", "유효하지 않은 심사 상태입니다."),
+    LOAN_APPLICATION_NOT_FOUND(HttpStatus.NOT_FOUND, "COMMON4004", "요청한 리소스를 찾을 수 없습니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
