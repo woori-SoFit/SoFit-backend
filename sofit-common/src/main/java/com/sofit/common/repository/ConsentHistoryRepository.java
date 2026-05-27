53cceb9dbcb56d3e7ab6a5320f84c64dd08ea752
minyeong <skaalsdud7@gmail.com>
2026-05-27 14:23:11 +0900

    [SOFIT-135] Refactor: 약관 조회 N+1 문제 해결

--- sofit-common/src/main/java/com/sofit/common/repository/ConsentHistoryRepository.java
+++ sofit-common/src/main/java/com/sofit/common/repository/ConsentHistoryRepository.java
@@ -2,6 +2,7 @@ package com.sofit.common.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -21,5 +22,6 @@ public interface ConsentHistoryRepository extends JpaRepository<ConsentHistory, 
 
     List<ConsentHistory> findByUser_UserIdOrderByConsentIdAsc(Long userId);
 
+    @EntityGraph(attributePaths = {"term"})
     List<ConsentHistory> findByUser_UserIdAndApplication_ApplicationIdOrderByConsentIdAsc(Long userId, Long applicationId);
 }
