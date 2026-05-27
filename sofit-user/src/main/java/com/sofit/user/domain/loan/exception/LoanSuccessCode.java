55426b650c32c620fe425702a3a046bb6b6e6432
HeeYeon-Ko <rhgmldus6@gmail.com>
2026-05-27 14:36:20 +0900

    [SOFIT-130] Refactor: LoanSuccessCode 번호 수정

--- sofit-user/src/main/java/com/sofit/user/domain/loan/exception/LoanSuccessCode.java
+++ sofit-user/src/main/java/com/sofit/user/domain/loan/exception/LoanSuccessCode.java
@@ -24,8 +24,8 @@ public enum LoanSuccessCode implements BaseSuccessCode {
     LOAN_RESUME_OK(HttpStatus.OK, "LOAN2011", "이어가기 데이터 조회에 성공했습니다."),
     LOAN_PRODUCT_OPTIONS_OK(HttpStatus.OK, "LOAN2012", "대출 상품 옵션 조회에 성공했습니다."),
     LOAN_STEP_CONSENT_OK(HttpStatus.OK, "LOAN2013", "대출 약관 동의가 완료되었습니다."),
-    LOAN_STEP_BIZ_INFO_OK(HttpStatus.OK, "LOAN2015", "사업자 정보 확인이 완료되었습니다."),
-    LOAN_STEP_MYDATA_OK(HttpStatus.OK, "LOAN2016", "마이데이터 약관 동의가 완료되었습니다.");
+    LOAN_STEP_BIZ_INFO_OK(HttpStatus.OK, "LOAN2014", "사업자 정보 확인이 완료되었습니다."),
+    LOAN_STEP_MYDATA_OK(HttpStatus.OK, "LOAN2015", "마이데이터 약관 동의가 완료되었습니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
