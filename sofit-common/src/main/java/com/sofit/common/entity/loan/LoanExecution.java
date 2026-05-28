4d11573f06bd7ebb0e2fd88e0156166a50100649
minyeong <skaalsdud7@gmail.com>
2026-05-28 09:39:24 +0900

    [SOFIT-138] Feat: LoanExecution entity 명세 변경 - bank_code 삭제

--- sofit-common/src/main/java/com/sofit/common/entity/loan/LoanExecution.java
+++ sofit-common/src/main/java/com/sofit/common/entity/loan/LoanExecution.java
@@ -34,6 +34,6 @@ public class LoanExecution {
     @Column(name = "account_number", nullable = false, length = 30)
     private String accountNumber;
 
-    @Column(name = "bank_code", nullable = false, length = 10)
-    private String bankCode;
+    // @Column(name = "bank_code", nullable = false, length = 10)
+    // private String bankCode;
 }
